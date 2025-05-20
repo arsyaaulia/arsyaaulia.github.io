c06c05b024e969321b1939c5271c75f6f8db57bf
Arsya Aulia <arsya1522@gmail.com>
2025-05-20 18:26:23 +0800

    modified

--- animation.js
+++ animation.js
@@ -19,23 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeBtn = document.getElementById('closeSidebar');
 
   toggleBtn.addEventListener('click', () => {
-    sidebar.classList.remove('translate-x-full');
-    sidebar.classList.add('translate-x-0');
+    sidebar.classList.remove('-translate-x-full');
+    sidebar.classList.add('-translate-x-0');
   });
 
   closeBtn.addEventListener('click', () => {
-    sidebar.classList.remove('translate-x-0');
-    sidebar.classList.add('translate-x-full');
+    sidebar.classList.remove('-translate-x-0');
+    sidebar.classList.add('-translate-x-full');
   });
-
-  window.addEventListener('scroll', ()=>{
-    if(scrollY > 50){
-        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
-    } else { 
-        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
-
-    }
-  })
 });
